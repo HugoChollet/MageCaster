7b305007c22d410e9900449a8088610f43bf6b97
HugoChollet <tibolescabo@gmail.com>
2024-05-03 15:52:18 +1000

    casting time and centered casting

--- Assets/Scripts/CastingActions.cs
+++ Assets/Scripts/CastingActions.cs
@@ -6,13 +6,34 @@ public class CastingActions : MonoBehaviour
     public Key castKey = Key.A;
     public GameObject cast;
 
+    float counter = 0;
+
+    private void Awake()
+    {
+        ;
+    }
+
     private void Update()
+    {
+        InstantiateCasting();
+    }
+
+    private void InstantiateCasting()
     {
         if (Keyboard.current.FindKeyOnCurrentKeyboardLayout(castKey.ToString()).isPressed)
         {
 
             Debug.Log(Input.inputString + " key was pressed.");
-            Instantiate(cast, transform.position, Quaternion.identity);
+            counter += Time.deltaTime;
+            if (counter > cast.GetComponent<Cast>().castingTime)
+            {
+                counter = 0;
+                Instantiate(cast, transform.position, Quaternion.identity);
+            }
+        }
+        else
+        {
+            counter = 0;
         }
     }
 }
