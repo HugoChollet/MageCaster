645a2098f8746f042d1b2ebfb24a93e0b5b9f37f
HugoChollet <tibolescabo@gmail.com>
2024-05-28 10:55:22 +1000

    update player prefab

--- Assets/Scripts/Casting/CastingInstantiate.cs
+++ Assets/Scripts/Casting/CastingInstantiate.cs
@@ -42,7 +42,6 @@ public class CastingInstantiate : MonoBehaviour
         if (playerStatus.manaStatus.UseStatus(cast.GetComponent<Cast>().manaCost))
         {
             Instantiate(cast, cast.GetComponent<Cast>().DetermineSpawn(), cast.GetComponent<Cast>().DetermineRotation());
-
         }
     }
 
