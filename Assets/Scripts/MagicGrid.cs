71211d0bc57985cd5d10d37dd8e869fe39d12c66
HugoChollet <tibolescabo@gmail.com>
2024-10-03 22:29:05 +0200

    add keypad input for magic selection

--- Assets/Scripts/MagicGrid.cs
+++ Assets/Scripts/MagicGrid.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.InputSystem;
 using System;
-using Unity.VisualScripting;
+using System.Collections.Generic;
 
 public class MagicGrid : MonoBehaviour
 {
@@ -25,7 +24,7 @@ public class MagicGrid : MonoBehaviour
             InstantiateMagicCell(magics[i], startScreenPositionX, i);
             startScreenPositionX += nextScreenPositionX;
         }
-        cells[roundDownSelectCell].GetComponentInChildren<Button>().Select();
+        cells[GetSelectedCell()].GetComponentInChildren<Button>().Select();
     }
 
     void InstantiateMagicCell(Magic magic, int posX, int index)
@@ -40,10 +39,11 @@ public class MagicGrid : MonoBehaviour
 
     void FixedUpdate()
     {
-        UpdateOnChangeSelect();
+        UpdateOnScroll();
+        UpdateOnKeyDown();
     }
 
-    void UpdateOnChangeSelect()
+    void UpdateOnScroll()
     {
         int oldSelect = roundDownSelectCell;
 
@@ -52,22 +52,59 @@ public class MagicGrid : MonoBehaviour
 
         if (oldSelect != roundDownSelectCell)
         {
-            SetSelectedMagics();
+            SetSelectedMagics(roundDownSelectCell);
         }
     }
 
 
-    public void SetSelectedMagics()
+    // Call Select on key press from 1 to 9
+    private Dictionary<KeyCode, int> digitMap = new Dictionary<KeyCode, int>()
+{
+    {KeyCode.Keypad0, 0}, {KeyCode.Alpha0, 0},
+    {KeyCode.Keypad1, 1}, {KeyCode.Alpha1, 1},
+    {KeyCode.Keypad2, 2}, {KeyCode.Alpha2, 2},
+    {KeyCode.Keypad3, 3}, {KeyCode.Alpha3, 3},
+    {KeyCode.Keypad4, 4}, {KeyCode.Alpha4, 4},
+    {KeyCode.Keypad5, 5}, {KeyCode.Alpha5, 5},
+    {KeyCode.Keypad6, 6}, {KeyCode.Alpha6, 6},
+    {KeyCode.Keypad7, 7}, {KeyCode.Alpha7, 7},
+    {KeyCode.Keypad8, 8}, {KeyCode.Alpha8, 8},
+    {KeyCode.Keypad9, 9}, {KeyCode.Alpha9, 9}
+};
+
+    void UpdateOnKeyDown()
+    {
+        if (Input.anyKey)
+        {
+            foreach (var key in digitMap)
+            {
+                if (Input.GetKey(key.Key))
+                {
+                    SetSelectedMagics(key.Value);
+                    return;
+                }
+            }
+        }
+    }
+
+
+    public void SetSelectedMagics(int selectedMagic)
     {
         for (int i = 0; i < cells.Length; i++)
         {
-            cells[i].GetComponent<MagicCell>().selected = i == roundDownSelectCell;
+            cells[i].GetComponent<MagicCell>().selected = i == selectedMagic;
             cells[i].GetComponent<MagicCell>().UpdateHighLight();
+            selectedCell = selectedMagic;
         }
+    }
+
+    public int GetSelectedCell()
+    {
+        return Math.Abs((int)(selectedCell % cells.Length));
     }
 
     public Magic GetSelectedMagic()
     {
-        return magics[roundDownSelectCell].GetComponent<Magic>();
+        return magics[GetSelectedCell()].GetComponent<Magic>();
     }
 }
